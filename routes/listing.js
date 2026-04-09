fb3eebab56220f3d6f360c744cda2badfd726f09
Harsh Sahu <harshsahu17102004@gmail.com>
2026-04-10 02:36:57 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require('../utils/wrapAsync.js');
+const Listing = require('../models/listing.js');
+const {isLoggedIn, isOwner, validateListing} = require('../middleware.js');
+const listingControllers = require('../controllers/listings.js');
+const multer  = require('multer');
+const {storage} = require('../cloudConfig.js');
+const upload = multer({ storage });
+
+//Index Route  //Create Route
+router
+    .route("/")
+    .get(wrapAsync(listingControllers.index))
+    .post(
+        isLoggedIn,
+        validateListing,
+        upload.single("listing[image]"),
+        wrapAsync(listingControllers.createListing)
+    );
+
+//New Route
+router.get("/new",isLoggedIn, listingControllers.renderNewForm);
+
+//Show Route  //Update Route //Delete Route
+router
+    .route("/:id")
+    .get(wrapAsync(listingControllers.showListing))
+    .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing, 
+    wrapAsync(listingControllers.updateListing)
+    )
+    .delete( 
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingControllers.destroyListing)
+    );
+
+//Edit Route
+router.get(
+    "/:id/edit",
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingControllers.renderEditForm));
+    
+module.exports = router;
