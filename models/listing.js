31b51f926e0ce6d6f041ac0c768e9ae21915d4bd
Harsh Sahu <harshsahu17102004@gmail.com>
2026-05-07 18:01:41 +0530

    basic

--- models/listing.js
+++ models/listing.js
@@ -5,23 +5,26 @@ const Review = require('./review.js');
 const listingSchema = new Schema({
     title: { type: String, required: true },
     description: String,
-    image: { url: String, filename: String },
+    image: {
+        url: { type: String, default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?w=800" },
+        filename: String
+    },
     price: Number,
     location: String,
     country: String,
     category: {
         type: String,
-        enum: ["trending", "rooms", "iconic-cities", "mountains", "castles", 
-               "amazing-pools", "camping", "farms", "arctic", "beaches", "desi-villas"],
+        enum: ["trending", "rooms", "iconic-cities", "mountains", "castles",
+            "amazing-pools", "camping", "farms", "arctic", "beaches", "desi-villas"],
     },
     reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
     owner: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
 // mongoose middleware
-listingSchema.post("findOneAndDelete", async(deletedListing) => {
-    if(deletedListing){
-        await Review.deleteMany({_id : {$in: deletedListing.reviews}});
+listingSchema.post("findOneAndDelete", async (deletedListing) => {
+    if (deletedListing) {
+        await Review.deleteMany({ _id: { $in: deletedListing.reviews } });
     }
 });
 
