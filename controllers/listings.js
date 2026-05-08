79f0a2ad22b9d100209a1a659f0fb832f6b3735a
Harsh Sahu <harshsahu17102004@gmail.com>
2026-05-08 21:03:40 +0530

    error

--- controllers/listings.js
+++ controllers/listings.js
@@ -79,7 +79,7 @@ module.exports.updateListing = async (req, res) => {
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
   if (typeof req.file !== "undefined") {
-    let url = req.file.path;
+    let url = req.file.secure_url || req.file.path;
     let filename = req.file.filename;
     listing.image = { url, filename};
     await listing.save();
